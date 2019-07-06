b41c2bd4ee78996aed99d2cafdd57dbdd7e46bb7
my <dalvit@red.local>
2019-07-06 17:12:32 +0900

    model add

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+# Create your models here.
 
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Post(BaseModel):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
